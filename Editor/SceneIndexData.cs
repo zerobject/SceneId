ec80575c44e70bc34177ffdbedafbeb48e73a9a5
zerobject <krutojc520@gmail.com>
2025-02-24 21:11:32 +0400

    Remove third-party dependencies

--- Editor/SceneIndexData.cs
+++ Editor/SceneIndexData.cs
@@ -1,4 +1,3 @@
-using NaughtyAttributes;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +6,6 @@ namespace Zerobject.SceneManagement.Editor
     [CreateAssetMenu(menuName = "Project/Editor/Scene Management/Scene Index Data")]
     public class SceneIndexData : ScriptableObject
     {
-        [ReadOnly] public List<SceneEntry> Entries = new();
+        public List<SceneEntry> Entries = new();
     }
 }
